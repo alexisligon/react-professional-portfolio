761895a64d641f1189118f3891e206831ee21498
Lex <aligon17@gmail.com>
2021-08-12 22:26:06 -0400

    wrote portfolioContainer.js code to render different pages and navtabs

--- main/src/components/PortfolioContainer.js
+++ main/src/components/PortfolioContainer.js
@@ -1,7 +1,34 @@
 import { useState } from 'react';
 import NavTabs from './NavTabs';
-import about from './pages/about';
-import contact from './pages/contact';
-import portfolio from './pages/portfolio';
-import resume from './pages/resume';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('About');
+
+    const renderPage = () => {
+        if (currentPage === 'About') {
+            return <About />
+        }
+        if (currentPage === 'Contact') {
+            return <Contact />
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />
+        }
+        if (currentPage === 'Resume') {
+            return <Resume />
+        }
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+        </div>
+    )
+}
