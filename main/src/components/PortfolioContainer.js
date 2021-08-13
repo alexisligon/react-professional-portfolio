f0f670ba4dc350e68c5d0408152653b923e824e2
Lex <aligon17@gmail.com>
2021-08-12 22:19:23 -0400

    added readme content, navtabs, and imported pages on portfolio contianer

---
+++ main/src/components/PortfolioContainer.js
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+import NavTabs from './NavTabs';
+import about from './pages/about';
+import contact from './pages/contact';
+import portfolio from './pages/portfolio';
+import resume from './pages/resume';
+
