82e7e75d86677e03da90b9d91b946fd4036c658b
Lex <aligon17@gmail.com>
2021-08-12 22:43:26 -0400

    restart react app, new code from class activity as starter

--- main/src/App.js
+++ main/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 
 const App = () => <PortfolioContainer />;
--- main/src/components/PortfolioContainer.js
+++ main/src/components/PortfolioContainer.js
@@ -1,34 +1,35 @@
 import { useState } from 'react';
 import NavTabs from './NavTabs';
+import Home from './pages/Home';
 import About from './pages/About';
+import Blog from './pages/Blog';
 import Contact from './pages/Contact';
-import Portfolio from './pages/Portfolio';
-import Resume from './pages/Resume';
 
 export default function PortfolioContainer() {
-    const [currentPage, setCurrentPage] = useState('About');
+  const [currentPage, setCurrentPage] = useState('Home');
 
-    const renderPage = () => {
-        if (currentPage === 'About') {
-            return <About />
-        }
-        if (currentPage === 'Contact') {
-            return <Contact />
-        }
-        if (currentPage === 'Portfolio') {
-            return <Portfolio />
-        }
-        if (currentPage === 'Resume') {
-            return <Resume />
-        }
-    };
+  // TODO: Add a comment describing the functionality of this method
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Blog') {
+      return <Blog />;
+    }
+    return <Contact />;
+  };
 
-    const handlePageChange = (page) => setCurrentPage(page);
+  const handlePageChange = (page) => setCurrentPage(page);
 
-    return (
-        <div>
-            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-            {renderPage()}
-        </div>
-    )
+  return (
+    <div>
+      {/* // TODO: Add a comment describing what we are passing as props */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* // TODO: Add a comment explaining what is happening on the following line */}
+      {renderPage()}
+    </div>
+  );
 }
