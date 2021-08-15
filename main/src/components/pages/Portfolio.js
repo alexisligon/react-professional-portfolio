5ae99ea6703fded59f78aef529408fa27e5ff089
Lex <aligon17@gmail.com>
2021-08-15 13:13:08 -0400

    added all images for projects, still needs styling

--- main/src/components/pages/Portfolio.js
+++ main/src/components/pages/Portfolio.js
@@ -1,9 +1,17 @@
 import books from '../../img/books.jpg';
+import clouds from '../../img/clouds.jpg';
+import food from '../../img/food.jpg';
+import password from '../../img/password.jpg';
+import planner from '../../img/planner.jpg';
 
 export default function Portfolio() {
   return (
     <div>
       <img src={books} alt="books"/>
+      <img src={clouds} alt="clouds"/>
+      <img src={food} alt="food"/>
+      <img src={password} alt="password"/>
+      <img src={planner} alt="planner"/>
     </div>
   )
   ;
