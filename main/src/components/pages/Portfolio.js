7a818d4c204bb90ebee17861dd0e44037e6065ef
Lex <aligon17@gmail.com>
2021-08-16 14:10:04 -0400

    resized portfolio project pics

--- main/src/components/pages/Portfolio.js
+++ main/src/components/pages/Portfolio.js
@@ -3,16 +3,38 @@ import clouds from '../../img/clouds.jpg';
 import food from '../../img/food.jpg';
 import password from '../../img/password.jpg';
 import planner from '../../img/planner.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function Portfolio() {
   return (
-    <div>
-      <img src={books} alt="books"/>
-      <img src={clouds} alt="clouds"/>
-      <img src={food} alt="food"/>
-      <img src={password} alt="password"/>
-      <img src={planner} alt="planner"/>
+    <div class="d-flex h-100 align-items-center">
+      <div class="container">
+        <div class="row">
+          <div>
+          <img src={books} alt="books" class="col-lg-4 col-md-6 mb-4 container"/>
+          </div>
+
+          <div>
+          <img src={clouds} alt="clouds" class="col-lg-4 col-md-6 mb-4 container"/>
+          </div>
+
+          <div>
+          <img src={food} alt="food" class="col-lg-4 col-md-6 mb-4 container"/>
+          </div>
+
+          <div>
+          <img src={password} alt="password" class="col-lg-4 col-md-6 mb-4 container"/>
+          </div>
+
+          <div>
+          <img src={planner} alt="planner" class="col-lg-4 col-md-6 mb-4 container"/>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
   ;
 }
+
