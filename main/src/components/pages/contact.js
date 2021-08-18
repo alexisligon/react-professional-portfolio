a8529b5a9ca2624324b06ce01b5f9c0ec12d40c7
Lex <aligon17@gmail.com>
2021-08-17 21:34:54 -0400

    deploying to github pages

--- main/src/components/pages/contact.js
+++ main/src/components/pages/contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Form from './form';
 
 export default function Contact() {
   return (
@@ -19,6 +20,7 @@ export default function Contact() {
           <a href="https://github.com/alexisligon">/alexisligon</a>
         </li>
       </ul>
+      <Form />
     </div>
   );
 }
