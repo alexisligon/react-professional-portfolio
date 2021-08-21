33af6b68b79bf810e81682bb7aef2ce9e3c9ad0d
Lex <aligon17@gmail.com>
2021-08-21 15:32:41 -0400

    styled grid for project images

--- src/components/pages/project.js
+++ src/components/pages/project.js
@@ -8,44 +8,56 @@ import planner from '../../img/planner.jpg';
 
 export default function ProjectCard() {
     return (
-        <div className="container">
-            <div className="row align-items-center">
-                <div className="col-lg-4 col-md-6 mb-4 container">
-                    <figure>
-                        <img src={books} alt="books" className="img-fluid d-block mx-auto" />
-                        <figcaption>Follow the Reader</figcaption>
-                    </figure>
-                </div>
 
-                <div className="col-lg-4 col-md-6 mb-4 container">
-                    <figure>
-                        <img src={clouds} alt="clouds" className="img-fluid d-block mx-auto" />
-                        <figcaption>Weather App</figcaption>
-                    </figure>
-                </div>
+        <div className="row row-cols-1 row-cols-sm-3 g-4">
 
-                <div className="col-lg-4 col-md-6 mb-4 container">
-                    <figure>
-                        <img src={food} alt="food" className="img-fluid d-block mx-auto" />
-                        <figcaption>Global Gastronomy</figcaption>
-                    </figure>
+            <div className="col">
+                <div className="card">
+                <img src={books} alt="books" className="card-img-top" />
+                <div className="card-body">
+                    <h5 className="card-title text-center"> Follow the Reader</h5>
                 </div>
-
-                <div className="col-lg-4 col-md-6 mb-4 container">
-                    <figure>
-                    <img src={password} alt="phonelock" className="img-fluid d-block mx-auto" />
-                    <figcaption>Password Generator</figcaption>
-                    </figure>
                 </div>
+            </div>
 
-                <div className="col-lg-4 col-md-6 mb-4 container">
-                    <figure>
-                    <img src={planner} alt="planner" className="img-fluid d-block mx-auto" />
-                    <figcaption>Day Planner</figcaption>
-                    </figure>
+            <div className="col">
+                <div className="card">
+                <img src={food} alt="food" className="card-img-top" />
+                <div className="card-body">
+                    <h5 className="card-title text-center"> GitHub Gastronomy</h5>
                 </div>
+                </div>
+            </div>
 
+            <div className="col">
+                <div className="card">
+                <img src={clouds} alt="storm clouds" className="card-img-top" />
+                <div className="card-body">
+                    <h5 className="card-title text-center"> Weather App</h5>
+                </div>
+                </div>
+            </div>
+
+            <div className="col">
+                <div className="card">
+                <img src={password} alt="phonelock" className="card-img-top" />
+                <div className="card-body">
+                    <h5 className="card-title text-center"> Password Generator</h5>
+                </div>
+                </div>
+            </div>
+
+            <div className="col">
+                <div className="card">
+                <img src={planner} alt="planner" className="card-img-top" />
+                <div className="card-body">
+                    <h5 className="card-title text-center"> Day Planner</h5>
+                </div>
+                </div>
             </div>
         </div>
+
+        
+
     )
 }
